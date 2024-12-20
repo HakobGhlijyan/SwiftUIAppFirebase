a45434d62353ad4f08d9e41ca44591597a8383cf
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-12-20 12:35:47 +0400

    Initial Commit

---
+++ SwiftUIAppFirebase/SwiftUIAppFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppFirebaseApp.swift
+//  SwiftUIAppFirebase
+//
+//  Created by Hakob Ghlijyan on 12/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
